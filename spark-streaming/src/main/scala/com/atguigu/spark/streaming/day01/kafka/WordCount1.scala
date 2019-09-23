210e2afd5eb90e568e9d8c28f2ec9775ccbfec7a
John Doe <30752740@qq.com>
2019-09-23 14:20:10 +0800

    更新

---
+++ spark-streaming/src/main/scala/com/atguigu/spark/streaming/day01/kafka/WordCount1.scala
@@ -0,0 +1,39 @@
+package com.atguigu.spark.streaming.day01.kafka
+
+import kafka.serializer.{StringDecoder, StringEncoder}
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * Author lzc
+  * Date 2019-09-23 11:38
+  */
+object WordCount1 {
+    def main(args: Array[String]): Unit = {
+        val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("HighKafka")
+        val ssc: StreamingContext = new StreamingContext(conf, Seconds(3))
+        // kafka 参数
+        //kafka参数声明
+        val brokers = "hadoop201:9092,hadoop202:9092,hadoop203:9092"
+        val topic = "first"
+        val group = "bigdata"
+        val kafkaParams = Map(
+            ConsumerConfig.GROUP_ID_CONFIG -> group,
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> brokers
+        )
+    
+        // 泛型12: kev,vlaue的类型   泛型34: keyvalue的解码器
+        val sourceDStream: InputDStream[(String, String)] = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
+            ssc,
+            kafkaParams,
+            Set(topic))
+        //
+        sourceDStream.print
+        
+        ssc.start()
+        ssc.awaitTermination()
+    }
+}
